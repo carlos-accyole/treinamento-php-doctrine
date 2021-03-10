9e047d9efd03c0f158e8a6ba258a9a12591e10be
Carlos Accyole <carlos_accyole@hotmail.com>
2021-03-09 23:43:31 -0300

    Primeiro Commit

---
+++ src/Entity/Aluno.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Entity
+ */
+class Aluno
+{
+
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private string $nome;
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+}
